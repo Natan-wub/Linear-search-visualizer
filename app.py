7bef0086ef94e94cc44a180c6bbab7c844677ca0
Natan-wub <24jw27@queensu.ca>
2025-12-05 22:52:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def visualize_linear_search(array_str, target_str):
         array = [int(x.strip()) for x in array_str.split(",")]
         target = int(target_str)
     except ValueError:
-        return None, "❌ Invalid input: Please enter only integers separated by commas."
-
+        return None, " >:( Invalid input: Please enter only integers separated by commas." #wrong input type leads to desplaying this message.
+#bar colors
     fig, ax = plt.subplots()
     bars = ax.bar(range(len(array)), array, color="lightblue")
     ax.set_title("Linear Search Visualization")
@@ -36,11 +36,11 @@ def visualize_linear_search(array_str, target_str):
             found_index = frame
             bars[frame].set_color("green")
 
-        # If this is the last frame and target not found → turn all red
+        # If this is the last frame and target not found then turn all red (implimented last)
         if frame == len(array) - 1 and found_index == -1:
             for bar in bars:
                 bar.set_color("red")
-
+#animation part
     ani = animation.FuncAnimation(
         fig, 
         update,  
@@ -56,9 +56,9 @@ def visualize_linear_search(array_str, target_str):
 
     # Return GIF + message
     if found_index == -1:
-        return gif_path, "❌ Target not found"
+        return gif_path, ":( Target not found"
     else:
-        return gif_path, f"✅ Target found at index {found_index}"
+        return gif_path, f":) Target found at index {found_index}"
 
 # Gradio interface: two outputs (image + text)
 demo = gr.Interface(
